90f227745f29245df37c56cd18172e3a96f0a433
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2018-03-06 08:28:19 +0100

    Add files via upload

---
+++ gitio.py
@@ -0,0 +1,34 @@
+import requests
+import argparse
+import sys
+
+API_URL = "https://git.io/create"
+HOME_URL = "https://git.io/"
+
+
+def shorten(url, custom_code=None):
+	data = requests.post(API_URL, data={"url":url}).text
+
+	print(HOME_URL+data)
+
+
+def main():
+	parser = argparse.ArgumentParser(description = "Create short GitHub url from the terminal")
+
+	parser.add_argument("-u", "--url",
+			type=str,
+			required=True,
+			help = "GitHub url")
+
+	args = parser.parse_args()
+
+
+	if len(sys.argv) == 1: # if argument is given then show help
+		parser.print_help()
+
+	elif args.url:
+		shorten(args.url)
+
+
+if __name__=="__main__":
+	main()
