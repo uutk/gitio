1f3c87816907b6ca234efa09ba468236e48066d6
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2018-03-06 16:02:11 +0100

    checks if there is internet

--- gitio.py
+++ gitio.py
@@ -4,13 +4,25 @@ import sys
 
 API_URL = "https://git.io/create"
 HOME_URL = "https://git.io/"
-ERROR = "Must be a GitHub.com URL."
 
-def shorten(url, custom_code=None):
-	data = requests.post(API_URL, data={"url":url}).text
+# Errors
+ERROR = "Must be a GitHub.com URL."
+INVALID_URL = "Invalid url:"
+
+def shorten(url):
+	# Note to self: test the "code" data to add a custom code
+	try:
+		data = requests.post(API_URL, data={"url":url}).text
+	except:
+		print("Please check your internet conection.")
+		exit()
 
 	if data == ERROR:
-		print(ERROR)
+		print(data)
+
+	elif INVALID_URL in data:
+		print(data)
+
 	else:
 		print(HOME_URL+data)
 
