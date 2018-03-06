470bee5482172733e923b49585aef1f113f91881
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2018-03-06 18:08:04 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name='gitio',
+    version='1.0',
+    description='Create short GitHub URLs from the terminal',
+    author='Siddharth Dushantha',
+    author_email='siddharth.dushantha@gmail.com',
+    url='https://github.com/sdushantha/gitio',
+    packages=['gitio'],
+    entry_points = {
+        'console_scripts': ['gitio=gitio.gitio:main'],
+    }
+)
